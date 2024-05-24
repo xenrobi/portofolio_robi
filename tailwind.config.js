e30a3954771e80964b36aecc40cab3e48b36b646
Robi <xenrobi@gmail.com>
2024-05-24 16:04:09 +0700

    dark mode and some adjust

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
-      padding : "16px",
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#06b6d4',
-        dark: '#0f172a',
-        secondary: '#64748b'
+        primary: "#06b6d4",
+        dark: "#0f172a",
+        secondary: "#64748b",
       },
       screens: {
-        '2xl': '1320px',
+        "2xl": "1320px",
       },
     },
   },
   plugins: [],
-}
-
+};
